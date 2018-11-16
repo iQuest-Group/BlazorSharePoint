b27f2b8736d1986150b8cb36994bd2859e20833c
ESP\lucian.ciurila <lucian.ciurila@iquestgroup.com>
2018-11-16 09:52:57 +0200

    First entities

---
+++ ScrabbleBlazor/ScrabbleBlazor.Shared/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrabbleBlazor.Shared
+{
+    public class Table
+    {
+        private TableSquare[,] table = new TableSquare[15, 15];
+
+        public TableSquare[,] Matrix
+        {
+            get
+            {
+                return table;
+            }
+        }
+
+        //public GetSquare(line, columns)
+        //{
+        //    return table[line][column]
+        //}
+    }
+}
---
+++ ScrabbleBlazor/ScrabbleBlazor.Shared/TableSquare.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrabbleBlazor.Shared
+{
+    public class TableSquare
+    {
+        public char Letter { get; set; } 
+    }
+}
